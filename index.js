200918ee52aa114f4bdc2fc14fdc78070074f506
Tani Kaye <1tanikaye@gmail.com>
2021-10-12 17:11:10 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let num5 = 1;
+let num6 = 2;
+let num7 = 3;
+let num8 = 20;
+let max = Math.max(num5, num6, num7, num8);
